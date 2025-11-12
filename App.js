a3820b6b7cb0849953ce6aa53087ad77fccad951
Ivy Christine Almarez <2301591@ub.edu.ph>
2025-11-12 23:00:55 +0800

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ActIndi from './ActivityIndicator';
+import Btn from './Button';
+import DisplayAnImage from './Image';
+import ImgBG from './ImageBackground';
+import KeyboardAvoidingComponent from './KeyboardAvoidingView';
+import Mod from './Modal';
+import Press from './Pressable';
+import Refresh from './RefreshControl';
+import Scroll from './ScrollView';
+import Sec from './SectionList';
+import List from './Simple FlatList';
+import Stat from './StatusBar Component';
+import Swtch from './Switch';
+import TextInANest from './Text Function Component ';
+import TextInputExample from './TextInput';
+import TouchableHighlightExample from './TouchableHighlight';
+import TouchOppa from './TouchableOpacity';
+import TouchableWithoutFeedbackExample from './TouchableWithoutFeedback';
+import ViewBoxesWithColorAndText from './View Example';
+import VirtList from './VirtualizedList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <>
+  {/* <ActIndi /> */}
+  
+  {/* <Btn /> */}
+
+  {/* <DisplayAnImage /> */}
+
+  {/* <ImgBG /> */}
+
+  {/* <KeyboardAvoidingComponent /> */}
+
+  {/* <Mod /> */}
+
+  {/* <Press /> */}
+
+  {/* <Refresh /> */}
+
+  {/* <Scroll /> */}
+
+  {/* <Sec /> */}
+
+  {/* <List /> */}
+
+  {/* <Stat /> */}
+
+  {/* <Swtch  /> */}
+
+  {/* <TextInANest /> */}
+
+  {/* <TextInputExample /> */}
+  
+  {/* <TouchableHighlightExample /> */}
+  
+  {/* <TouchOppa /> */}
+
+  {/* <TouchableWithoutFeedbackExample /> */}
+
+  {/* <ViewBoxesWithColorAndText /> */}
+
+  {/* <VirtList /> */}
+
+  </>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
